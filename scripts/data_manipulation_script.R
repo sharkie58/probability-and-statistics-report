7f8d4ec167d8faa913c2e8b595f42c6f693484a9
Sarka <s1861053@sms.ed.ac.uk>
2025-11-10 22:36:45 +0000

    check for normal distributions in random variables

--- scripts/data_manipulation_script.R
+++ scripts/data_manipulation_script.R
@@ -12,7 +12,6 @@ library(janitor) # for cleaning column names
 biomarkers <- read_excel("data/biomarkers.xlsx")
 covariates <- read_excel("data/covariates.xlsx")
 
-
 # Explore data ------------------------------------------------------------
 biomarkers
 covariates
@@ -80,6 +79,34 @@ sprintf("There are %s NAs in full_dataset_clean.", nas)
 
 # Find the NAs
 colSums(is.na(full_dataset_clean)) # 2 missing values in VAS after 12 months.
+
+# Check distributions -----------------------------------------------------
+
+female <- full_dataset_clean %>%
+  filter(sex == 2)
+
+hist(female$il_8) # normal
+hist(female$vegf_a) # normal
+hist(female$opg) # normal
+hist(female$tgf_beta_1) # not normal
+hist(female$il_6) # right skew
+hist(female$cxcl9) # right skew
+hist(female$cxcl1) # left skew
+hist(female$il_18) # normal
+hist(female$csf_1) # normal
+
+male <- full_dataset_clean %>%
+  filter(sex == 1)
+
+hist(male$il_8) # normal
+hist(male$vegf_a) # normal
+hist(male$opg) # normal
+hist(male$tgf_beta_1) # not normal
+hist(male$il_6) # right skew
+hist(male$cxcl9) # right skew
+hist(male$cxcl1) # left skew
+hist(male$il_18) # not normal
+hist(male$csf_1) # normal
 
 # Save data ---------------------------------------------------------------
 write.csv(full_dataset_clean, "data/biomarkers_covariates_clean.csv")
