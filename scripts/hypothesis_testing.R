d3c0ef970a966cef246422c1c38d63a5e168f38e
Sarka <s1861053@sms.ed.ac.uk>
2025-11-11 19:51:50 +0000

    refactor: perform all 9 t-tests in a pipe

--- scripts/hypothesis_testing.R
+++ scripts/hypothesis_testing.R
@@ -4,6 +4,9 @@
 
 # Load packages ---------------------------------------------------------------
 library(dplyr) # for pipes and manipulation
+library(broom) # for tidy test results
+library(gtsummary) # for publication ready tables
+library(rstatix) # for a t_test (wrapper for t.test)
 
 # Load data -------------------------------------------------------------------
 biomarker <- read.csv("data/biomarkers_covariates_clean.csv")
@@ -24,81 +27,35 @@ biomarker <- read.csv("data/biomarkers_covariates_clean.csv")
 
 # Use the Welch test to compare the means of each of 9 biomarkers
 
-# 1. Biomarker IL-8
-biomarker_test_il_8 <- t.test(il_8 ~ sex, biomarker)
-biomarker_test_il_8 # H1 p-value 0.33
+# To perform t-tests in a single loop, convert data from wide to long format 
+# The script to perform all 9 tests together was taken from Datanovia (2020).
+biomarker_long <- biomarker %>%
+  select(il_8:csf_1,sex) %>% # remove columns that won't be used in testing
+  pivot_longer(-sex, names_to = "biomarkers", values_to = "value")
 
-# 2. Biomarker VEGF-A
-biomarker_test_vegf_a <- t.test(vegf_a ~ sex, biomarker)
-biomarker_test_vegf_a # H1 p-value 0.04
+# See the long dataset
+biomarker_long
 
-# 3. Biomarker OPG
-biomarker_test_opg <- t.test(opg ~ sex, biomarker)
-biomarker_test_opg # H1 p-value 0.13
+# Construct Welch t-tests for all 9 biomarkers
+hypotheses_test <- biomarker_long %>%
+  group_by(biomarkers) %>%
+  t_test(value ~ sex)
 
-# 4. Biomarker TGF-beta-1
-biomarker_test_tgf_beta_1 <- t.test(tgf_beta_1 ~ sex, biomarker)
-biomarker_test_tgf_beta_1 # H1 p-value 0.05
+# See test results
+hypotheses_test
 
-# 5. Biomarker IL-6
-biomarker_test_il_6 <- t.test(il_6 ~ sex, biomarker)
-biomarker_test_il_6 # H1 p-value 0.25
-
-# 6. Biomarker CXCL9
-biomarker_test_cxcl9 <- t.test(cxcl9 ~ sex, biomarker)
-biomarker_test_cxcl9 # H1 p-value 0.98
-
-# 7. Biomarker CXCL1
-biomarker_test_cxcl1 <- t.test(cxcl1 ~ sex, biomarker)
-biomarker_test_cxcl1 # H1 p-value 0.01
-
-# 8. Biomarker IL-18
-biomarker_test_il_18 <- t.test(il_18 ~ sex, biomarker)
-biomarker_test_il_18 # H1 p-value 0.25
-
-# 9. Biomarker CSF-1
-biomarker_test_csf_1 <- t.test(csf_1 ~ sex, biomarker)
-biomarker_test_csf_1 # H1 p-value 0.01
 
 # Bonferroni Correction ------------------------------------------------------
 
 # The probability of making a Type I error is 0.05 across all 9 t-tests, lowering 
 # the p-value necessary to reach significance for each individual test to 0.006.
 
-# 1. Biomarker IL-8
-biomarker_test_il_8_corr <- t.test(il_8 ~ sex, biomarker, conf.level = 0.994)
-biomarker_test_il_8_corr
+hypotheses_test_bonferroni <- biomarker_long %>%
+  group_by(biomarkers) %>%
+  t_test(value ~ sex) %>%
+  adjust_pvalue(method = "bonferroni")
 
-# 2. Biomarker VEGF-A
-biomarker_test_vegf_a_corr <- t.test(vegf_a ~ sex, biomarker, conf.level = 0.994)
-biomarker_test_vegf_a_corr
+hypotheses_test_bonferroni
 
-# 3. Biomarker OPG
-biomarker_test_opg_corr <- t.test(opg ~ sex, biomarker, conf.level = 0.994)
-biomarker_test_opg_corr 
-
-# 4. Biomarker TGF-beta-1
-biomarker_test_tgf_beta_1_corr <- t.test(tgf_beta_1 ~ sex, biomarker, conf.level = 0.994)
-biomarker_test_tgf_beta_1_corr 
-
-# 5. Biomarker IL-6
-biomarker_test_il_6_corr <- t.test(il_6 ~ sex, biomarker, conf.level = 0.994)
-biomarker_test_il_6_corr
-
-# 6. Biomarker CXCL9
-biomarker_test_cxcl9_corr <- t.test(cxcl9 ~ sex, biomarker, conf.level = 0.994)
-biomarker_test_cxcl9_corr 
-
-# 7. Biomarker CXCL1
-biomarker_test_cxcl1_corr <- t.test(cxcl1 ~ sex, biomarker, conf.level = 0.994)
-biomarker_test_cxcl1_corr 
-
-# 8. Biomarker IL-18
-biomarker_test_il_18_corr <- t.test(il_18 ~ sex, biomarker, conf.level = 0.994)
-biomarker_test_il_18_corr 
-
-# 9. Biomarker CSF-1
-biomarker_test_csf_1_corr <- t.test(csf_1 ~ sex, biomarker, conf.level = 0.994)
-biomarker_test_csf_1_corr 
-
-# None of the tests are significant
+# None of the tests fulfill significance for the alternative hypothesis with
+# Bonferroni correction.
